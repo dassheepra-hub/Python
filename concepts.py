b39f83af17212fb7cea4eb518562c47cc28cf1ce
Sheepra_Das <dassheepra@gmail.com>
2026-04-19 22:54:12 +0530

    Python concepts

--- concepts.py
+++ concepts.py
@@ -1,4 +1,9 @@
 a = 10
 print(a)
 a = a+2
-print(a)
+print(a) #a is a variable and its value is changed with a+2
+name = {"Hello" , "World"}
+print(name)
+print(type(name))
+print("hello","world")
+
